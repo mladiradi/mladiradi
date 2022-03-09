d2a6585e082a094277263bf9640221f61aaa9348
mladiradi <100635431+mladiradi@users.noreply.github.com>
2022-03-09 13:39:08 +0200

    Add files via upload

---
+++ Sutrin.java
@@ -0,0 +1,23 @@
+public class Sutrin {
+    public static void main( String[] args) {
+    System.out.println("Добро утро!");
+    System.out.println("Отивам в кухнята.");
+    System.out.println("Ще пия ли кафе, това е въпросът?");
+    System.out.println("Да, ще пия кафе!");    
+    System.out.println("-правя си кафе,");
+    System.out.println("-изпивам си кафето.");
+    System.out.println("Не,няма да пия кафе!");
+    System.out.println("Обличам се");
+    System.out.println("Излизам!");
+    System.out.println("Ще закусвам ли, отново този въпос?");
+    System.out.println("Да,ще закусвам!");
+    System.out.println("-правя си закуска,");
+    System.out.println("-закусвам,");
+    System.out.println("Обличам се");
+    System.out.println("Излизам!");
+    System.out.println("Не,няма да закусвам!");
+    System.out.println("Обличам се");
+    System.out.println("Излизам!");
+
+}
+}
